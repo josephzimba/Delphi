dffed5ab54126d4e89401810a08d9f292a5c2ef3
Joseph Zimba <zimba@ualberta.ca>
2020-04-28 16:56:09 -0600

    Added Company, Occupation and Staff Models

---
+++ Delphi/Models/Company.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Delphi.Models
+{
+    public class Company
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Contact { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Address { get; set; }
+
+    }
+}
---
+++ Delphi/Models/Staff.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Delphi.Models
+{
+    public class Staff
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public int SIN { get; set; }
+    }
+}
