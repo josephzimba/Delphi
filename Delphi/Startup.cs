f9d1b014522b7d3460c227e53d02d828ccc0a18b
Joseph Zimba <zimba@ualberta.ca>
2020-04-28 16:13:22 -0600

    First commit

---
+++ Delphi/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Delphi.Startup))]
+namespace Delphi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
